e27e4dba5f5798b61580dec3f1bca285c6c1c3f7
EasonChen <492926400@qq.com>
2022-03-17 00:41:32 +0800

    feat: add chrome dp screen shot

--- app/controller/activity_controller.go
+++ app/controller/activity_controller.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"ginson/pkg/log"
+	"github.com/chromedp/chromedp"
+	"github.com/chromedp/chromedp/device"
 	"github.com/gin-gonic/gin"
 	"github.com/skip2/go-qrcode"
 )
@@ -33,4 +35,30 @@ func (c *ActivityController) GetQrCode(ctx *gin.Context) {
 		return
 	}
 	_, _ = ctx.Writer.Write(png)
+}
+
+func (c *ActivityController) GetScreenShot(ctx *gin.Context) {
+	// create context
+	chromedpCtx, cancel := chromedp.NewContext(ctx)
+	defer cancel()
+
+	chromedp.WithLogf(func(s string, i ...interface{}) {
+		log.Info(s, i)
+	})
+
+	var buf []byte
+	if err := chromedp.Run(chromedpCtx, c.fullScreenshot(`https://www.baidu.com/`, 100, &buf)); err != nil {
+		c.FailedWithErr(ctx, err)
+		return
+	}
+	_, _ = ctx.Writer.Write(buf)
+}
+
+func (c *ActivityController) fullScreenshot(url string, quality int, res *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Emulate(device.IPhoneXR),
+		chromedp.Navigate(url),
+		//chromedp.EvaluateAsDevTools(`p = document.querySelector("#hotsearch-refresh-btn > span");p.innerText="我是傻逼";`, nil),
+		chromedp.FullScreenshot(res, quality),
+	}
 }
--- pkg/router/activity_router.go
+++ pkg/router/activity_router.go
@@ -17,4 +17,5 @@ func (c *ActivityRouter) BindRoutes(group *gin.RouterGroup) {
 	//group.Use(middleware.TokenMiddleware(service.GetTokenService()))
 	group.POST("/get-prize", c.controller.GetPrize, middleware.TokenMiddleware(service.GetTokenService()))
 	group.GET("/get-qr-code", c.controller.GetQrCode)
+	group.GET("/get-screenshot", c.controller.GetScreenShot)
 }
